68ec500d4804d7bf640308235e8ce959b25dd5bb
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-15 16:42:39 +0100

    try-catch

--- src/test.jsx
+++ src/test.jsx
@@ -1,14 +1,18 @@
 const urls = [
-  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicodee.com/posts',
   'https://jsonplaceholder.typicode.com/albums',
   'https://jsonplaceholder.typicode.com/users',
 ]
 
 const getData = async function () {
-  const [posts, albums, users] = await Promise.all(
-    urls.map((url) => fetch(url).then((resp) => resp.json()))
-  )
-  console.log('posts', posts)
-  console.log('albums', albums)
-  console.log('users', users)
+  try {
+    const [posts, albums, users] = await Promise.all(
+      urls.map((url) => fetch(url).then((resp) => resp.json()))
+    )
+    console.log('posts', posts)
+    console.log('albums', albums)
+    console.log('users', users)
+  } catch {
+    console.log('nono')
+  }
 }
