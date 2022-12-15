d258c9f8c2c94b368adf18c188dfc70fad54fbcb
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-15 16:08:38 +0100

    async-await-test

--- src/test.jsx
+++ src/test.jsx
@@ -1,5 +1,14 @@
-async function fetchUsers() {
-  const resp = await fetch('https://jsonplaceholder.typicode.com/users')
-  const data = await resp.json()
-  console.log(data)
+const urls = [
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums',
+  'https://jsonplaceholder.typicode.com/users',
+]
+
+const getData = async function () {
+  const [posts, albums, users] = await Promise.all(
+    urls.map((url) => fetch(url).then((resp) => resp.json()))
+  )
+  console.log('posts', posts)
+  console.log('albums', albums)
+  console.log('users', users)
 }
