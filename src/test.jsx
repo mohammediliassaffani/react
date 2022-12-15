589a072121cadc8d2ab21a75b8ac352ffcf20419
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-15 18:01:38 +0100

    final

--- src/test.jsx
+++ src/test.jsx
@@ -1,18 +1,7 @@
-const urls = [
-  'https://jsonplaceholder.typicodee.com/posts',
-  'https://jsonplaceholder.typicode.com/albums',
-  'https://jsonplaceholder.typicode.com/users',
-]
-
-const getData = async function () {
-  try {
-    const [posts, albums, users] = await Promise.all(
-      urls.map((url) => fetch(url).then((resp) => resp.json()))
-    )
-    console.log('posts', posts)
-    console.log('albums', albums)
-    console.log('users', users)
-  } catch {
-    console.log('nono')
-  }
-}
+const promiseOne = new Promise((resolve, reject) => {
+  setTimeout(resolve, 3000)
+})
+const promiseTow = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000)
+})
+Promise.all([promiseOne, promiseTow]).then((data) => console.log(data))
