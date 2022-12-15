76c91c1e85b83d5e102335e9c2aa3f0743c9cdcf
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-15 15:14:58 +0100

    test-fetch

--- src/test.jsx
+++ src/test.jsx
@@ -1,15 +1,3 @@
-const urls = [
-  'https://jsonplaceholder.typicode.com/posts',
-  'https://jsonplaceholder.typicode.com/albums',
-  'https://jsonplaceholder.typicode.com/users',
-]
-
-Promise.all(
-  urls.map((url) => {
-    return fetch(url).then((resp) => resp.json())
-  })
-).then((Results) => {
-  console.log(Results[0])
-  console.log(Results[1])
-  console.log(Results[2])
-})
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((resp) => resp.json())
+  .then(console.log())
